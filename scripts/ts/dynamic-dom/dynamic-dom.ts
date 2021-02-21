6b0477f347e51350139bf33d3d558ddb92288655
23pinapples <59811161+23pinapples@users.noreply.github.com>
2021-02-20 22:44:28 -0600

    edit
    added drop down faq

--- scripts/ts/dynamic-dom/dynamic-dom.ts
+++ scripts/ts/dynamic-dom/dynamic-dom.ts
@@ -9,6 +9,23 @@ export function PutStudentPageLoadOperationsInsideThisStudentBody() {
     // TODO: Put all operations that you want to happen on ever page load in this function.
     // For example you could write: Sticky.setup()
     doSomething();
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    console.log("different");
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        console.log("something");
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+    }
+    
+
 }
 
 export async function setupAll() {
---
+++ scripts/ts/dynamic-dom/faq_drop.ts
@@ -0,0 +1,16 @@
+
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+// console.log("different");
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     console.log("something");
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     } 
+//   });
+// }
